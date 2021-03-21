d88f4bbe46ff13d1663d1ded2e6cfb91c92f8ac4
shaffic <mshaffic@gmail.com>
2021-03-21 21:44:39 +0300

    adding comment to post

--- app/src/main/java/com/ensibuuko/test/ui/main/Utils.java
+++ app/src/main/java/com/ensibuuko/test/ui/main/Utils.java
@@ -3,11 +3,14 @@ package com.ensibuuko.test.ui.main;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class Utils {
 
     public static final String MyPREFERENCES = "PreferenceInfo";
-    public static final String USER_OBJECT_KEY = "user";
+    public static final String USERNAME_KEY = "user_name";
+    public static final String NAME_KEY = "name";
     public static final String KEY_FIRSTTIME = "first_time_launch";
 
     public static void save(String key, String value, Context context){
@@ -27,9 +30,26 @@ public class Utils {
         return prefs.getBoolean(key, true);
     }
 
+    public static String getStringKey(String key, Context context){
+        SharedPreferences prefs = getSharedPreferencesWithin(context);
+        return prefs.getString(key, "");
+    }
+
     private static SharedPreferences getSharedPreferencesWithin(Context context) {
 
         return context.getSharedPreferences(
                 MyPREFERENCES, Context.MODE_PRIVATE);
+    }
+
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
